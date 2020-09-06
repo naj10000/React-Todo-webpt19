64d04068e7fe7c1e6cfed0d75786c717a06924d9
NaJ <naj10000@live.com>
2020-09-06 12:29:42 -0400

    setting up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm'
+
+const todos = [
+  {
+    task: 'meditate',
+    id: 123,
+    completed: false
+  },
+  {
+    task: 'workout',
+    id: 124,
+    completed: false
+  },
+  {
+    task: 'take cold shower',
+    id: 125,
+    completed: false
+  },
+
+  {
+    task: 'do code challenges',
+    id: 126,
+    completed: false
+  }
+
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(){
+    super();
+    this.state = {
+      todos
+    }
+  }
+
   render() {
     return (
       <div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import Todo from './Todo'
+
+
+
+export default function TodoList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
